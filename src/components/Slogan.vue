1721495ad3e077478dd5d58c559fa10f7c0edf34
Ackerven <ackerven@gmail.com>
2025-11-06 08:48:10 +0800

    feat: add animation to slogan

--- src/components/Slogan.vue
+++ src/components/Slogan.vue
@@ -26,4 +26,29 @@
 
 <style scoped>
 
+.jumper {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: clamp(36px, 8vw, 90px);
+  white-space: nowrap;
+  color: #000;
+  user-select: none;
+}
+
+.jumper-char {
+  display: inline-block;
+  animation: bounce 0.3s ease infinite alternate;
+  will-change: transform;
+}
+
+@keyframes bounce {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, -0.5em, 0)
+  }
+}
+
 </style>
